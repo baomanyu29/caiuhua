5f8acc32a09df2910d18b11c4a37261d1a6aec97
sigboat <sigboat@sigboatdeMac-mini-2.local>
2014-04-11 10:02:48 +0800

    Initial Commit

---
+++ 采花大道/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  采花大道
+//
+//  Created by sigboat on 14-4-11.
+//  Copyright (c) 2014年 sigboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 采花大道Tests/____Tests.m
@@ -0,0 +1,34 @@
+//
+//  ____Tests.m
+//  采花大道Tests
+//
+//  Created by sigboat on 14-4-11.
+//  Copyright (c) 2014年 sigboat. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ____Tests : XCTestCase
+
+@end
+
+@implementation ____Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
